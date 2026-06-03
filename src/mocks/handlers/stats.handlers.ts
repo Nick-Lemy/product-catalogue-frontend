157158e3ccf11ff5b826e7dc8b5740a50fcf172f
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-03 11:47:11 +0200

    refactor: remove artificial delay in stats API handler

--- src/mocks/handlers/stats.handlers.ts
+++ src/mocks/handlers/stats.handlers.ts
@@ -2,8 +2,7 @@ import { HttpResponse, http } from "msw";
 import { mockDashboardStats } from "../data/stats";
 
 export const statsHandlers = [
-  http.get("/api/stats", async () => {
-    await new Promise((r) => setTimeout(r, 2000));
+  http.get("/api/stats", () => {
     return HttpResponse.json(mockDashboardStats);
   }),
 ];
