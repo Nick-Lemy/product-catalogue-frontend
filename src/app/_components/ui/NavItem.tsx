2bd3f0964000f62ca7888f1e8b6bdc66a0b5e824
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-04 12:10:44 +0200

    refactor: simplify active state management in NavItem component

--- src/app/_components/ui/NavItem.tsx
+++ src/app/_components/ui/NavItem.tsx
@@ -2,7 +2,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 
 function NavItem({
   icon: Icon,
@@ -13,13 +12,9 @@ function NavItem({
   label: string;
   href: string;
 }) {
-  const [isActive, setIsActive] = useState(false);
-
   const route = usePathname();
 
-  useEffect(() => {
-    setIsActive(route === href);
-  }, [route, href]);
+  const isActive = route === href;
 
   return (
     <Link href={href}>
